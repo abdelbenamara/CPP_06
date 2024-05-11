6982cd3a79d4935fc7c7f7d0a09d8c6bf57d96c2
Abdel Benamara <abdelb.contact@gmail.com>
2024-05-11 10:34:35 +0200

    Added Serializer

---
+++ ex01/Serializer.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 00:37:27 by abenamar          #+#    #+#             */
+/*   Updated: 2024/05/11 01:32:49 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+Serializer::Serializer(void) throw() { return; }
+
+Serializer::Serializer(Serializer const & /* src */) throw() { return; }
+
+Serializer::~Serializer(void) throw() { return; }
+
+Serializer &Serializer::operator=(Serializer const & /* rhs */) throw() { return (*this); }
+
+uintptr_t Serializer::serialize(Data *ptr) throw()
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *Serializer::deserialize(uintptr_t raw) throw()
+{
+	return reinterpret_cast<Data *>(raw);
+}
